65d168ae00031d575cb2453d56c66eee72b4f8dd
Florian <arnoud.florian.malgor@gmail.com>
2020-09-06 18:34:02 +0200

    Second commit

--- exos 3 4 5 et 6 .py
+++ exos 3 4 5 et 6 .py
@@ -1,11 +1,100 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Sep  6 15:01:48 2020
+from math import  *
 
-@author: agrab
-"""
-z=-540
-print (((z/-100)*((z%1)*200)))
+#N=int(input("taille de votre sapin : "))
+#def sapin(x):
+#    if x!= 0:
+#        print(x*' '+((N-x)*2+1)*'^')
+#        sapin(x-1)
+#
+#sapin(N)
+
+#-------------------------------------------
+#
+#def HT (z):
+#    n=1
+#    while n!=0:
+#        ttc=z*1.20
+#        print("Le prix TTC de",z,"€","est de :",ttc,"€")
+#        n=int(input("pour arreter le script taper 0, sinon taper un autre prix HT pour avoir son prix TTC : "))
+#        if n==0:
+#            print("le script est terminer")
+#        while n!=0:
+#            print("le prix TTC est de : ", n*1.20,"€")
+#            n=int(input("pour arreter le script taper 0, sinon taper un autre prix HT pour avoir son prix TTC : "))
+#
+#print(HT(50))
+
+#-------------------------------------------
+#p=int(input("nombre de poule tué par le chasseur : "))
+#c=int(input("nombre de chien tué par le chasseur : "))
+#v=int(input("nombre de vache tué par le chasseur : "))
+#a=int(input("nombre d' ami tué par le chasseur : "))
+#z=100
+#def amende(p,c,v,a,z):
+#    z=z-(p*1+c*3+v*5+a*10)
+#    if z >0:
+#        print("il reste ",z,"points au chasseur, il n'a donc pas perdu son permis")
+#    elif z<0 :
+#        print("Le chasseur a perdu",-z,"points","il a donc perdu son permis",(z/-100),"fois, il doit donc payer",ceil(((z/-100)*200-((z/-100)%1)*200)),"€ d'amende")
+#    elif z==0:
+#        print("Le chasseur a perdu 100 points, il doit donc payer  200 € d'ammende")
+#
+#print(amende(p,c,v,a,z))
 
 
-#((z/-100)*200)-
+#-------------------------------------------
+
+#p=int(input("Votre poids : "))
+#q=int(input("Quandtité de nourriture que vous mangez : "))
+#i=int(input("nombre d'annimaux dans la comparaison : "))
+#
+#for i in range (1,i+1):
+#    z[i]=int(input("poid de l'animal ",(i)))
+#    y[i]=int(input("quantité de nourriture pour l'animal",(i)))
+#    a[i+1]=int(input("poid de l'animal ",(i+1)))
+#    x[i+1]=int(input("quantité de nourriture pour l'animal",(i+1)))
+
+
+#-------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
